a2f188df0f4464e5bb0e7eaa1e27952abcfc5566
Alexander Kiko <elkikos@gmail.com>
2024-01-16 21:53:16 +0100

    MISSING;

--- nixos/luks-btrfs.nix
+++ nixos/luks-btrfs.nix
@@ -1,39 +1,40 @@
 {
-  disko.devices = {
-    disk = {
-      vdb = {
-        type = "disk";
-        device = "/dev/vda";
-        content = {
-          type = "gpt";
-          partitions = {
-            ESP = {
-              size = "512M";
-              type = "EF00";
-              content = {
-                type = "filesystem";
-                format = "vfat";
-                mountpoint = "/boot";
-                mountOptions = [
-                  "defaults"
-                ];
-              };
-            };
-            luks = {
-              size = "100%";
-              content = {
-                type = "luks";
-                name = "crypted-root";
-                passwordFile = "/tmp/secret.key"; 
-                settings = {
-                  allowDiscards = true;
+    disko.devices = {
+      disk = {
+        vdb = {
+          type = "disk";
+          device = "/dev/vda";
+          content = {
+            type = "gpt";
+            partitions = {
+              ESP = {
+                size = "512M";
+                type = "EF00";
+                content = {
+                  type = "filesystem";
+                  format = "vfat";
+                  mountpoint = "/boot";
+                  mountOptions = [
+                    "defaults"
+                  ];
                 };
-              content = {
-                type = "btrfs";
-                mountpoint = "/";
-                mountOptions = ["noatime"];
-                subvolumes = {
-                  "/home" = {};
+              };
+              luks = {
+                size = "100%";
+                content = {
+                  type = "luks";
+                  name = "crypted-root";
+                  passwordFile = "/tmp/secret.key"; 
+                  settings = {
+                    allowDiscards = true;
+                  };
+                content = {
+                  type = "btrfs";
+                  mountpoint = "/";
+                  mountOptions = ["noatime"];
+                  subvolumes = {
+                    "/home" = {};
+                  };
                 };
               };
             };
@@ -41,5 +42,5 @@
         };
       };
     };
-  };
+  }
 }
