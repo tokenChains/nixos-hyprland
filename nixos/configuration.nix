f3c4dc43292291aa3d977b42c868f851242a321b
Alexander Kiko <elkikos@gmail.com>
2024-01-16 22:01:16 +0100

    fixed errors

--- nixos/configuration.nix
+++ nixos/configuration.nix
@@ -7,6 +7,8 @@
   imports =
     [ # Include the results of the hardware scan.
       ./hardware-configuration.nix
+      "${builtins.fetchTarball "https://github.com/nix-community/disko/archive/master.tar.gz"}/module.nix"
+      ./luks-btrfs.nix
       ./fonts.nix
       ./apps.nix
       ./virtualization.nix
