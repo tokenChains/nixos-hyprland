75eab0196c45b9e2bd7fd49b468b75a67ec6cfdd
Alexander Kiko <elkikos@gmail.com>
2024-01-16 21:46:20 +0100

    edit disk

---
+++ nixos/luks-btrfs.nix
@@ -0,0 +1,45 @@
+{
+  disko.devices = {
+    disk = {
+      vdb = {
+        type = "disk";
+        device = "/dev/vda";
+        content = {
+          type = "gpt";
+          partitions = {
+            ESP = {
+              size = "512M";
+              type = "EF00";
+              content = {
+                type = "filesystem";
+                format = "vfat";
+                mountpoint = "/boot";
+                mountOptions = [
+                  "defaults"
+                ];
+              };
+            };
+            luks = {
+              size = "100%";
+              content = {
+                type = "luks";
+                name = "crypted-root";
+                passwordFile = "/tmp/secret.key"; 
+                settings = {
+                  allowDiscards = true;
+                };
+              content = {
+                type = "btrfs";
+                mountpoint = "/";
+                mountOptions = ["noatime"];
+                subvolumes = {
+                  "/home" = {};
+                };
+              };
+            };
+          };
+        };
+      };
+    };
+  };
+}
